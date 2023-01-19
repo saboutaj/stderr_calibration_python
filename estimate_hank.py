2f6426dbf4a8a44eaef1f34f2c2d58ee57201dae
Samya Aboutajdine <113407930+saboutaj@users.noreply.github.com>
2023-01-19 16:40:55 +0100

    New coefficients

--- estimate_hank.py
+++ estimate_hank.py
@@ -138,7 +138,7 @@ def moment_fct(theta):
 
 # Estimation using diagonal weight matrix
 param_bounds = [(0,np.inf),(1e-6,np.inf),(-np.inf,np.inf),(-np.inf,np.inf),(0,np.inf),(-np.inf,np.inf),(-np.inf,np.inf)] # Parameter bounds
-param_init = np.array([1.352,0.144,0.951,0,0.072,0.460,0]) # Initial parameter guess (from Auclert et al.)
+param_init = np.array([1.352,0.144,-0.0251,-0.0239,0.0728,0.460,0]) # Initial parameter guess (from Auclert et al. + transf)
 
 print('Running numerical optimization for diagonal weight matrix...')
 opt_res = opt.minimize(lambda theta: np.sum(((moment-moment_fct(theta))/moment_se)**2),
